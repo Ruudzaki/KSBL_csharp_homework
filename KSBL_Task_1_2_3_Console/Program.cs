14446dcdf2f1769994fb1ddeff24df21a21afc78
Ruudzaki <ksbl@simcorp.com>
2019-10-16 15:45:03 +0300

    minor changes, added Task 1.2.3.

---
+++ KSBL_Task_1_2_3_Console/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KSBL_Task_1_2_3_Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string firstString = "\nmy string 1";
+            string secondString = "\tmy string 2";
+            string thirdString = "\amy string 3";
+
+            Console.WriteLine(firstString);
+            Console.WriteLine(secondString);
+            Console.WriteLine(thirdString);
+
+            Console.ReadKey();
+        }
+    }
+}
