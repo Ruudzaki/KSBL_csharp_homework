efde18d8398c03478e0204d5e3e865c11b32e810
Ruudzaki <ksbl@simcorp.com>
2019-10-16 15:27:17 +0300

    Task121

---
+++ KSBL_Task_1_2_1_Console/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KSBL_Task_1_2_1_Console
+{
+    
+    class Program
+    {
+        static readonly double pi = 3.141592653;
+        static readonly decimal e = 2.7182818284590452M;
+
+        static void Main(string[] args)
+        {
+            double firstNumber = pi;
+            decimal secondNumber = e;
+
+            Console.WriteLine(firstNumber);
+            Console.WriteLine(secondNumber);
+
+            Console.ReadKey();
+        }
+    }
+}
