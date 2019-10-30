bb3ed510f70fbe3a9a05bd5a10a42b13ea06c3e6
Ruudzaki <ksbl@simcorp.com>
2019-10-30 12:49:30 +0200

    added classwork 301019

---
+++ KSBL_Classwork_301019/Cow.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KSBL_Classwork_301019
+{
+    class Cow : Animal
+    {
+        public Cow(string name)
+        {
+            Name = name;
+        }
+
+        public override void Eat()
+        {
+            
+            Console.WriteLine("I eat only grass");
+        }
+        public override void Voice()
+        {
+            Console.WriteLine("Moo-moo");
+        }
+    }
+}
---
+++ KSBL_Classwork_301019/IPet.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KSBL_Classwork_301019
+{
+    interface IPet
+    {
+        void Pet();
+    }
+}
