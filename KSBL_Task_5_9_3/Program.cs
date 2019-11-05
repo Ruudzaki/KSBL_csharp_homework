9a0c0b88a934c59d9fd48b79f64116e438a1d26a
Ruudzaki <56171632+Ruudzaki@users.noreply.github.com>
2019-11-05 20:10:11 +0200

    added tasks 5.9. ended C# Starter

---
+++ KSBL_Task_5_9_3/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KSBL_Task_5_9_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = new int[10];
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = i;
+            }
+
+            int[] arrayR = MyReverse(array);
+
+            for (int i = 0; i < arrayR.Length; i++)
+            {
+                Console.WriteLine(arrayR[i]);
+            }
+
+            Console.WriteLine();
+
+            int[] subArray = SubArray(array, 5, 10);
+
+            for (int i = 0; i < subArray.Length; i++)
+            {
+                Console.WriteLine(subArray[i]);
+            }
+
+            Console.ReadKey();
+        }
+
+        private static int[] MyReverse(int[] array)
+        {
+            int[] arrayR = new int[array.Length];
+
+            for (int i = 0, j = arrayR.Length - 1; i < array.Length; i++, j--)
+            {
+                arrayR[j] = array[i];
+            }
+
+            return arrayR;
+        }
+
+        private static int[] SubArray(int[] array, int index, int count)
+        {
+            int[] subArray = new int[count];
+
+            for (int i = 0; i < count; i++, index++)
+            {
+                if (index < array.Length)
+                {
+                    subArray[i] = array[index];
+                }
+                else
+                {
+                    subArray[i] = 1;
+                }
+            }
+
+            return subArray;
+        }
+    }
+}
